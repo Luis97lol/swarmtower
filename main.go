402b16b49cea690cb6be811ecb848a67404c0107
Ju-Wiluis William Rodriguez Hernandez <jwrodriguezh.externo@bcds.net>
2024-10-16 14:39:55 +0200

    Test

--- main.go
+++ main.go
@@ -1,21 +1,50 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os/exec"
+	"strings"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/gorilla/mux"
 )
+
+func removeDigest(imageName string) string {
+	parts := strings.Split(imageName, "@")
+	return parts[0] // Retorna solo la parte antes del digest
+}
 
 func updateService(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	serviceName := vars["servicename"] // Obtener el nombre del servicio de la ruta
 	log.Println("Recibida peticion para actualizar el servicio:", serviceName)
 
+	// Crear cliente Docker usando el socket por defecto
+	cli, err := client.NewClientWithOpts(client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Println("Error al conectar con Docker:", err.Error())
+		http.Error(w, "Error al conectar con Docker", http.StatusInternalServerError)
+		return
+	}
+	defer cli.Close()
+
+	// Obtener el servicio por nombre
+	service, _, err := cli.ServiceInspectWithRaw(context.Background(), serviceName, types.ServiceInspectOptions{})
+	if err != nil {
+		log.Println("Error al obtener detalles del servicio:", err.Error())
+		http.Error(w, "Error al obtener detalles del servicio", http.StatusInternalServerError)
+		return
+	}
+
+	currentImageDigest := service.Spec.TaskTemplate.ContainerSpec.Image
+	log.Println("Digest de la imagen actual:", currentImageDigest)
+
 	// Comando para hacer pull de la imagen y actualizar el servicio
-	pullCmd := exec.Command("docker", "service", "update", "--force", serviceName)
+	pullCmd := exec.Command("docker", "service", "update", "--force", "--image", removeDigest(currentImageDigest), serviceName)
 
 	// Ejecutar el comando y obtener la salida
 	output, err := pullCmd.CombinedOutput()
